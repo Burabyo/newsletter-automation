4435b5fda33f3b8eef6518926eff9d04e479e79a
Burabyo <j.burabyo@alustudent.com>
2026-02-18 16:22:06 +0200

    add utils

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,31 +1,51 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import utils.ExtentManager;
+import utils.ScreenshotUtil;
 
 import java.time.Duration;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected ExtentReports extent;
+    protected ExtentTest test;
 
     @BeforeEach
-    public void setup() {
+    public void setup(TestInfo testInfo) {
+
+        extent = ExtentManager.getInstance();
+        test = extent.createTest(testInfo.getDisplayName());
+
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+
         driver.get("https://burabyo.github.io/Newsletter-sign-up-form/");
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown(TestInfo testInfo) {
+
         if (driver != null) {
             driver.quit();
         }
+
+        extent.flush();
+    }
+
+    protected void captureFailure(String testName) {
+        String path = ScreenshotUtil.captureScreenshot(driver, testName);
+        test.fail("Test Failed").addScreenCaptureFromPath(path);
     }
 }
-
--- src/test/java/pages/NewsletterPage.java
+++ src/test/java/pages/NewsletterPage.java
@@ -4,38 +4,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class NewsletterPage {
 
-    WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     public NewsletterPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     // Locators
-
     @FindBy(id = "email")
-    WebElement emailInput;
+    private WebElement emailInput;
 
     @FindBy(className = "submit-btn")
-    WebElement submitButton;
+    private WebElement submitButton;
 
     @FindBy(className = "error-message")
-    WebElement errorMessage;
+    private WebElement errorMessage;
 
     @FindBy(id = "modal")
-    WebElement successModal;
+    private WebElement successModal;
 
     @FindBy(id = "confirm-email")
-    WebElement confirmEmail;
+    private WebElement confirmEmail;
 
     @FindBy(id = "dismiss-btn")
-    WebElement dismissButton;
+    private WebElement dismissButton;
 
-    // Actions
+    @FindBy(id = "newsletter-form")
+    private WebElement newsletterForm;
 
+    // --- Actions ---
     public void enterEmail(String email) {
         emailInput.clear();
         emailInput.sendKeys(email);
@@ -45,19 +52,64 @@ public class NewsletterPage {
         submitButton.click();
     }
 
-    public String getErrorMessage() {
-        return errorMessage.getText();
+    public void submitWithEnterKey() {
+        emailInput.sendKeys(org.openqa.selenium.Keys.ENTER);
     }
 
+    public void dismissModal() {
+        wait.until(ExpectedConditions.elementToBeClickable(dismissButton));
+        dismissButton.click();
+        wait.until(ExpectedConditions.invisibilityOf(successModal));
+    }
+
+    // --- Getters ---
+    public WebElement getEmailInput() {
+        return emailInput;
+    }
+
+    public WebElement getSubmitButton() {
+        return submitButton;
+    }
+
+    public WebElement getErrorMessageElement() {
+        return errorMessage;
+    }
+
+    public WebElement getSuccessModal() {
+        return successModal;
+    }
+
+    public WebElement getConfirmEmailElement() {
+        return confirmEmail;
+    }
+
+    public WebElement getDismissButton() {
+        return dismissButton;
+    }
+
+    public WebElement getNewsletterForm() {
+        return newsletterForm;
+    }
+
+    // --- Helper Methods ---
     public boolean isModalDisplayed() {
+        wait.until(ExpectedConditions.visibilityOf(successModal));
         return successModal.isDisplayed();
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return errorMessage.isDisplayed();
     }
 
     public String getConfirmedEmail() {
         return confirmEmail.getText();
     }
 
-    public void dismissModal() {
-        dismissButton.click();
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+
+    public String getEmailValue() {
+        return emailInput.getAttribute("value");
     }
 }
